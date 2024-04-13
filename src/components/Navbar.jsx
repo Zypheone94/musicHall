ff7686ef35874750ba8515f5a4cd4e491584a97f
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-04-13 09:20:00 +0200

    EVO - (Auth0) : Setting up a rudimentary connection and disconnection system

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import styles from '../styles/button.module.css';
+import React from "react";
+import styles from "../styles/button.module.css";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
-    <div className='flex w-full justify-between items-center h-20'>
-        <div>Logo</div>
-        <nav>
-            <ul className='flex list-none'>
-                <li>Search</li>
-                <li>Cours</li>
-                <li>Download</li>
-                <li>About us</li>
-            </ul>
-        </nav>
-        <div id='social' className='flex'>
-            <span>F</span>
-            <span>G</span>
-            <span>I</span>
-        </div>
-        <button className={`${styles.button} text-white`}>Login</button>
+    <div className="flex w-full justify-between items-center h-20">
+      <div>Logo</div>
+      <nav>
+        <ul className="flex list-none">
+          <li>Search</li>
+          <li>Cours</li>
+          <li>Download</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+      <div id="social" className="flex">
+        <span>F</span>
+        <span>G</span>
+        <span>I</span>
+      </div>
+      {user ? (
+        <a href="/api/auth/logout">
+          <button className={`${styles.button} text-white`}>Logout</button>
+        </a>
+      ) : (
+        <a href="/api/auth/login">
+          <button className={`${styles.button} text-white`}>Login</button>
+        </a>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // Get the user data
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-      <main
-        id="container"
-        className={`flex min-h-screen flex-col items-center justify-between px-20 ${inter.className}`}
-      >
-        <div id="app-body" className="w-full">
-          <Navbar />
+    <main
+      id="container"
+      className={`flex min-h-screen flex-col items-center justify-between px-20 ${inter.className}`}
+    >
+      <div id="app-body" className="w-full">
+        <Navbar user={user} />
+      </div>
+      {user && (
+        <div>
+          <p>Bonjour : {user.name}</p>
+          <img src={user.picture} alt={`${user.name}-profile-pic`} />
         </div>
-      </main>
+      )}
+    </main>
   );
 }
